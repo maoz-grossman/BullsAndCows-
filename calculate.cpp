101d35f275847ef2d39377bf515a5ab1cf8a4801
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2019-06-02 21:58:06 +0300

    fix

--- calculate.cpp
+++ calculate.cpp
@@ -1,38 +1,51 @@
 #include "calculate.hpp"
 #include <string>
+#include <stdexcept>
+#include <iostream>
 using std::string, std::to_string;
-
+using namespace std;
 namespace bullpgia {
 	const string calculateBullAndPgia(const string choosen, const string guess){
-		if(choosen.length() != guess.length())
-        return "0,0";
 
-    else{
         uint bull = 0;
         uint pgia = 0;
+		uint size= guess.length();
 
-        for(int i = 0; i<=guess.length(); i++){
-            if(choosen.find(guess[i])){
-                for(int j = 0; j<=choosen.length(); j++){
-                    bool flag = false;
-                    if(guess[i] == choosen[j]){
-                        if(i==j){
-                            bull++;
-                            if(flag)
-                            pgia--;
-                        break;
-                            }
-                        else if(!flag){
+		if(size>choosen.length())
+			size=choosen.length();
+
+ 		bool f_guess[guess.length()];
+ 		bool f_chooser[choosen.length()];
+		
+ 		for(uint i=0;i<choosen.length();i++){
+ 			f_chooser[i]=false;
+ 		}
+		for(uint i=0;i<guess.length();i++){
+ 			f_guess[i]=false;
+ 		}
+
+ 		for(uint i = 0; i<size; i++){
+ 			if(choosen[i]==guess[i]){
+ 				f_chooser[i]=true;
+ 				f_guess[i]=true;
+ 				bull++;
+ 			}
+ 		}
+
+        for(int i = 0; i<choosen.length(); i++){
+        	if(f_chooser[i]!=true){
+                for(int j = 0; j<guess.length(); j++){
+                    if(choosen[i]==guess[j]){
+                    	if(f_guess[j]!=true){
                             pgia++;
-                            flag=true;
-                            }
-                    }
-                }
+                            f_guess[j]=true;
+                            break;
+        	}
             }
         }
+    }
+    }
         	std::string s=(to_string(bull)+","+to_string(pgia));
          return s;
     }
-	}
-
 	}
