3edde926a6564feee9dbdb761086351036cbc010
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2019-06-02 21:53:21 +0300

    fix

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -2,20 +2,22 @@
 #include "Guesser.hpp"
 #include "calculate.hpp"
 #include <unordered_set>
+#include <iostream>
 
 
 /**
- * ConstantGuesser is a guesser that always guesses the same string. 
+ * SmartGuesser is a guesser that guesses the secret in maximum 100 turns.
  */
 class SmartGuesser: public bullpgia::Guesser {
 	private:
-		std::unordered_set<std::string> myset;
-		std::string lastGuess;
-
+		std::string Guess="";
+		std::string tempguess = "";//temporery guess to check witch number are in the secret
+		std::unordered_set<std::string> _set;
+		int guess_index=0;// help varible to go oer all numbers (0-9)
+		void Guess_changer();// help function to change the tempguess
+		void permute(std::string str); // help function to create all prmutations of tempguess
 	public:
 		std::string guess() override;
-		void startNewGame(uint theLength) override;
-		void learn(std::string response) override;
+		void startNewGame(uint Length) override;
+		void learn(std::string res) override;
 };
-
-std::string numToGuess(int num, uint length);
