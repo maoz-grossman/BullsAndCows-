866ceaa754624e3d3ea5d1e86a69c3b0646258cc
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2019-06-02 21:51:49 +0300

    ;-)

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,21 +1,86 @@
 #include "SmartGuesser.hpp"
-#include <math.h>
+#include "calculate.hpp"
+#include <unordered_set>
 #include <iostream>
-
+#include <bits/stdc++.h>
 
 using std::string, std::to_string, std::unordered_set;
 string SmartGuesser::guess() {
-	return "";
+	return this->Guess;
 }
 
-void SmartGuesser::startNewGame(uint theLength) {
-	
-}
-void SmartGuesser::learn(string response) {
-	
+//---------------------------------//
+/*--------------new game-----------*/
+//---------------------------------//
+void SmartGuesser::startNewGame(uint Length) {
+	Guess="";
+    _set.clear();//clear our set if it already full
+	tempguess="";
+	guess_index=0;
+   this->length=Length;
+  for(int i=0;i<this->length;i++)// initialize first guess with '0'
+  	Guess+='0';
 }
 
-//convert number to guess type by padding "0"
-string numToGuess(int num, uint length){
-	return "";
+
+//------------------------------//
+/*------improves our guess------*/
+//------------------------------//
+
+void SmartGuesser::learn(string res) {
+	if(tempguess.length()<this->length){
+		int sum = (res.at(0)-'0');
+		for(int i=0;i<sum;i++)
+			tempguess+=to_string(guess_index);
+		guess_index++;
+		if(guess_index>9)
+			guess_index=0;
+		Guess_changer();
+	}
+	else
+	{
+		if(_set.empty())
+			permute(tempguess);
+	
+	char bulls= res.at(0);//checks how many bulls we have
+	char cows= res.at(2);//checks how many cows we have
+	unordered_set<string> NotTheAnswer;
+	for ( auto it = _set.begin(); it != _set.end(); it++){
+		string response = bullpgia::calculateBullAndPgia(*it,Guess);
+		if (response.at(0)!=bulls)
+			NotTheAnswer.insert(*it);	
+	}
+
+	//removes from _set
+	for ( auto it = NotTheAnswer.begin(); it != NotTheAnswer.end(); it++)
+		_set.erase(*it);
+	this->Guess= *_set.begin();
+	}
+}
+
+
+//--------------------------------//
+/*--change the temporary guess--*/
+//------------------------------//
+void SmartGuesser::Guess_changer(){
+	for(int i=0;i<this->length;i++){
+		Guess.replace(i,i+1,to_string(guess_index));
+	}
+}
+
+
+//------------------------------------//
+/*----all possible premutetion of----*/
+//------------------------------------//
+void SmartGuesser::permute(string str) 
+{ 
+    // Sort the string in lexicographically 
+    // ascennding order 
+    sort(str.begin(), str.end()); 
+  
+    // Keep printing next permutation while there 
+    // is next permutation 
+    do { 
+      _set.insert(str);
+    } while (std::next_permutation(str.begin(), str.end())); 
 }
